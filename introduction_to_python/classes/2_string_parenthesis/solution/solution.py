46487a52c6e047489e4bc03605c622c05083ac39
mattlam88 <63694045+mattlam88@users.noreply.github.com>
2020-08-23 23:52:14 -0400

    updated exercises 8/23/20

--- introduction_to_python/classes/2_string_parenthesis/solution/solution.py
+++ introduction_to_python/classes/2_string_parenthesis/solution/solution.py
@@ -1,3 +1,11 @@
 class py_solution:
-   def is_valid_parenthese(self, str):
-        return
+   def is_valid_parenthese(self, str1):
+      brackets,container = [], {"(": ")", "{": "}", "[": "]"}
+      
+      for paranthese in str1:
+         if paranthese in container:
+            brackets.append(paranthese)
+         elif len(brackets) == 0 or container[brackets.pop()] != paranthese:
+            return False
+
+      return len(brackets) == 0
