358ab64630489a0ec9a259a5dcd41e93b5ea44be
mattlam88 <63694045+mattlam88@users.noreply.github.com>
2020-08-30 22:57:02 -0400

    Linked list exercises 8/30

--- data_structures/linked_lists/1_delete_end/solution/solution.py
+++ data_structures/linked_lists/1_delete_end/solution/solution.py
@@ -1,1 +1,17 @@
-# Write your answers here
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class linkedlist:
+    def __init__(self, head=None):
+        self.head = head
+
+    def delete_at_end(self):
+        if self.head == None:
+            return None
+        x = self.head
+        while x.next.next is not None:
+            x = x.next
+        x.next = None
--- data_structures/linked_lists/1_insert_after/solution/solution.py
+++ data_structures/linked_lists/1_insert_after/solution/solution.py
@@ -1,1 +1,16 @@
-# Write your answers here
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class linkedList:
+    def __init__(self, head = None):
+        self.head = head
+
+    def insert_after_item(self, x, data):
+        if self.head == None:
+            return None
+        n = self.head
+
+        
