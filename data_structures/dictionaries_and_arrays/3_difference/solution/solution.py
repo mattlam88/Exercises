02555f6c743b90c8c9d433e582b015fd67e2596a
mattlam88 <63694045+mattlam88@users.noreply.github.com>
2020-08-30 22:41:25 -0400

    dictionaries and arrays exercises 8/30

--- data_structures/dictionaries_and_arrays/3_difference/solution/solution.py
+++ data_structures/dictionaries_and_arrays/3_difference/solution/solution.py
@@ -1,1 +1,3 @@
-# Write your solution here
+def difference(array_num):
+    num_list = sorted(array_num)
+    return num_list[-1] - num_list[0]
--- data_structures/dictionaries_and_arrays/3_square_keys/solution/solution.py
+++ data_structures/dictionaries_and_arrays/3_square_keys/solution/solution.py
@@ -1,1 +1,5 @@
-# Write your solution here
+def square_keys(num):
+    square_list = {}
+    for i in range(1,num + 1):
+        square_list[i] = i ** 2
+    return square_list
